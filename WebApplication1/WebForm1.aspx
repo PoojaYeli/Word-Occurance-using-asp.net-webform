312f0691c6cde0a5a4cdb7f36c58c346ba430907
PoojaYeli <puja.v.y@gmail.com>
2018-11-16 17:05:04 +0100

    Add files via upload

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Async="true" Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        jQuery(document).ready(function () {
+            $("#parseFile").on("click", function(){
+                var uploadedFile = $("#uploadFile")[0].files;
+                if (uploadedFile.length == 0) {
+                alert("Please select a file!");
+                }
+            });
+        });
+    </script>
+    <style>
+        #wordOccurance{
+            margin-top: 15px;
+            border: solid thin black;
+        }
+        #wordOccurance th, #wordOccurance td{
+            text-align: center;
+            border: solid thin black;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        
+        <div style="margin: auto;width: 60%;padding: 10px;margin-top: 0px;">
+            <fieldset>
+                <legend>Add a text file to check the word count</legend>
+                <div style="margin-bottom:5px;">
+                    <asp:FileUpload ID="uploadFile" runat="server" Height="21px" />
+                </div>
+                <asp:Button ID="parseFile" runat="server" OnClick="ParseFile_click" Text="Click here" />
+                <asp:Button ID="cancelFileParse" runat="server" OnClick="cancelParseFile_click" Text="Cancel" />
+            </fieldset>
+            <hr />
+            <div runat="server" id="wordCountTable">
+                <asp:Table ID="wordOccurance" runat="server">
+                <asp:TableHeaderRow>
+                    <asp:TableHeaderCell>Word</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Occurence</asp:TableHeaderCell>
+                </asp:TableHeaderRow>
+                </asp:Table>
+            </div>
+            <span runat="server" id="displayIfEmptyFile"></span>
+                   
+        </div>
+    </form>
+    </body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            wordOccurance.Visible = false;
+            wordCountTable.Visible = false;
+        }
+
+        void ReportProgress(int value)
+        {
+            //this.progressPanel.Attributes["aria-valuenow"] = value.ToString();
+        }
+
+        public List<KeyValuePair<string, int>> checkAsync(string filePath, IProgress<int> progress, CancellationToken ct)
+        {
+            Dictionary<string, int> contentCountDict = new Dictionary<string, int>();
+            String fileData = File.ReadAllText(filePath);
+
+            var contentCountList = new List<KeyValuePair<string, int>>();
+            String[] fileWords = Regex.Split(fileData, @"\s");
+            int totalFileWords = fileWords.Length;
+            int totalWordsProcessed = 0;
+
+            //sort the word count in eference with cont
+            foreach (string fileWord in fileWords)
+            {
+                if (!string.IsNullOrWhiteSpace(fileWord))
+                {
+                    if (contentCountDict.ContainsKey(fileWord))
+                    {
+                        contentCountDict[fileWord] += 1;
+                    }
+                    else
+                    {
+                        contentCountDict.Add(fileWord, 1);
+                    }
+                    totalWordsProcessed++;
+                    //cancel the file process
+                    ct.ThrowIfCancellationRequested();
+                    //update the progress
+                    progress.Report(value: totalWordsProcessed * 100 / totalFileWords);
+                    //Thread.Sleep(300);
+                }
+            }
+
+            contentCountList = contentCountDict.ToList();
+            contentCountList.Sort((content1, content2) => content1.Value.CompareTo(content2.Value));
+            //Return the sorted word count list
+            return contentCountList;
+        }
+
+        public async void ParseFile_click(object sender, EventArgs e)
+        {
+            wordOccurance.Visible = false;
+            wordCountTable.Visible = false;
+            if (uploadFile.HasFile)
+            {
+                String filePath = Server.MapPath("~/userFiles/") + uploadFile.FileName;
+                //Save the uploaded file in userFiles folder
+                var fileExt = Path.GetExtension(uploadFile.PostedFile.FileName);
+                if (fileExt.Equals(".txt"))
+                {
+                    uploadFile.SaveAs(filePath);
+
+                    Progress<int> progressIndicator = new Progress<int>(ReportProgress);
+                    try
+                    {
+                        var contentCountList = await Task.Run(() => checkAsync(filePath, progressIndicator, cts.Token));
+                        wordOccurance.Visible = true;
+                        wordCountTable.Visible = true;
+                        displayIfEmptyFile.InnerText = "";
+                        //Append the sorted list to the table
+                        foreach (var fileContentPair in contentCountList)
+                        {
+                            KeyValuePair<string, int> fileContent = fileContentPair;
+                            TableRow row = new TableRow();
+                            TableCell contentCell = new TableCell();
+                            TableCell contentCountCell = new TableCell();
+                            contentCell.Text = fileContent.Key;
+                            row.Cells.Add(contentCell);
+                            contentCountCell.Text = fileContent.Value.ToString();
+                            row.Cells.Add(contentCountCell);
+                            wordOccurance.Rows.Add(row);
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        //process cancelled
+                    }
+                }
+                else
+                {
+                    displayIfEmptyFile.InnerText = "Please upload a text file";
+                }
+            }
+            else
+            {
+                displayIfEmptyFile.InnerText = "Either the file is empty or their was some error processing";
+            }
+        }
+
+        public void cancelParseFile_click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+    }
+}
